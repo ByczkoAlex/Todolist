4adfcc36236fa47d1e6bfa0eccdea2d889dbc1fb
Alex <byczko2012@mail.ru>
2021-08-20 12:43:32 +0300

    Updates

---
+++ precache-manifest.9dce6e4f819f87368d1f6fbd3e729fc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bdd3739b2ae4f63ae187e2ab76b36fa",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "ff901e290cf4f133fbd9",
+    "url": "/Todolist/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "b853e9c1599992e97f8f",
+    "url": "/Todolist/static/js/2.1f608c14.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/Todolist/static/js/2.1f608c14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff901e290cf4f133fbd9",
+    "url": "/Todolist/static/js/main.778f468a.chunk.js"
+  },
+  {
+    "revision": "8c9ba947015c2dd63600",
+    "url": "/Todolist/static/js/runtime-main.e8babc40.js"
+  }
+]);
